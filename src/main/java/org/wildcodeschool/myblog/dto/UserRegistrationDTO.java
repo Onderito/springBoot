26530982bd52b0401111c53d400b562f9f35687d
2z75 <zinedine.chader@gmail.com>
2024-09-19 17:34:38 +0200

    Quete 12

---
+++ src/main/java/org/wildcodeschool/myblog/dto/UserRegistrationDTO.java
@@ -0,0 +1,15 @@
+package org.wildcodeschool.myblog.dto;
+
+public class UserRegistrationDTO {
+
+    private String email;
+    private String password;
+
+
+    //Getters et Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
